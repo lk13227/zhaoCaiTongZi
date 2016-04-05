7272b676070b64000d4fcf90a95f3db3e61da2db
zhibin <zhibinhsu@163.com>
2016-04-05 17:17:00 +0800

    Initial Commit

---
+++ zhaoCaiTongZi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhaoCaiTongZi
+//
+//  Created by longmen1 on 16/4/5.
+//  Copyright © 2016年 XZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhaoCaiTongZi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhaoCaiTongZi
+//
+//  Created by longmen1 on 16/4/5.
+//  Copyright © 2016年 XZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
